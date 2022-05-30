e8f0bc7a4594ec49812f932da1caea5b6a060e65
popovmihail <husky-74@yandex.ru>
2022-05-30 14:44:04 +0400

    Create project

---
+++ AirTicketPriceProject/src/main/java/com/myIdea/AirTicketPrice/AirTicketPriceApplication.java
@@ -0,0 +1,12 @@
+package com.myIdea.AirTicketPrice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirTicketPriceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AirTicketPriceApplication.class, args);
+    }
+    
+}
